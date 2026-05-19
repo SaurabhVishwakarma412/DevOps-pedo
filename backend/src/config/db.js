3dea80a7ac445562d04c82607d086141ef96385f
saurabh <saurabhkv412@gmail.com>
2026-05-19 18:39:22 +0530

    updated db name

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
+  const dbName = "pedoderma_admin";
+
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(`${process.env.MONGO_URI}/${dbName}`);
     console.log("MongoDB Connected");
   } catch (error) {
     console.error("Mongo DB Connection Failed:", error.message);
